47c6396e3215598ad9f8dc1a2d93f8293dee2d8f
MatejDragicevic <matej.dragicevic.work@gmail.com>
2022-05-20 18:34:19 +0200

    LC1 two sum

--- kWeakestRows.java
+++ kWeakestRows.java
@@ -1,39 +1,39 @@
-import java.util.*;
+import java.util.Arrays;
 
-class Solution {
-    
+class KWeakestRows {
+
     public static int leftOnes(int[] array) {
         int low = 0;
         int high = array.length;
-        
+
         while (low < high) {
             int mid = low + (high - low) / 2;
-            
-            if(array[mid] == 1)
+
+            if (array[mid] == 1)
                 low = mid + 1;
             else
                 high = mid;
         }
         return low;
     }
-    
+
     public int[] kWeakestRows(int[][] mat, int k) {
         int rows = mat.length;
         int cols = mat[0].length;
-        
+
         int[] rowValue = new int[rows];
         int j = 0;
-        
-        for(int i = 0; i<rows; i++){
+
+        for (int i = 0; i < rows; i++) {
             j = leftOnes(mat[i]);
             rowValue[i] = j * rows + i;
         }
-        
+
         Arrays.sort(rowValue);
-        for(int i = 0; i<k; i++) {
+        for (int i = 0; i < k; i++) {
             rowValue[i] = rowValue[i] % rows;
         }
-        
+
         return Arrays.copyOfRange(rowValue, 0, k);
     }
 }
