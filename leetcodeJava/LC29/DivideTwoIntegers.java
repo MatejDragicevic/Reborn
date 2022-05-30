0600bb9630178b3513e5d45cdd314aeea68e4247
MatejDragicevic <matej.dragicevic.work@gmail.com>
2022-05-30 16:33:26 +0200

    LC29 update

--- leetcodeJava/LC29/DivideTwoIntegers.java
+++ leetcodeJava/LC29/DivideTwoIntegers.java
@@ -1,23 +1,25 @@
 class Solution {
     public int divide(int dividend, int divisor) {
+        // can't deal with that
         boolean divisionWithZero = divisor == 0;
-        boolean minValueDividedWithOne = dividend==Integer.MIN_VALUE&&divisor==-1;
-        if(divisionWithZero || minValueDividedWithOne) return Integer.MAX_VALUE;
-        int quotient = 0;
-        boolean oneNegativOnePositiv = (dividend < 0) ^ (divisor < 0);
-        int sign = oneNegativOnePositiv ? -1 : 1;
+        boolean minValueDividedWithNegativeOne = dividend==Integer.MIN_VALUE&&divisor==-1;
+        if(divisionWithZero || minValueDividedWithNegativeOne) return Integer.MAX_VALUE;
+
         long longDividend = Math.abs((long) dividend);
         long longDivisor = Math.abs((long) divisor);
-        while(longDivisor <= longDividend){
-            long temp = longDivisor;
-            long multiplicator =1;
-            while(longDividend >= temp<<1){
-                temp <<= 1;
-                multiplicator <<= 1;
+        int quotient = 0;
+        // division in bits
+        while(longDividend >= longDivisor){
+            long divisorShiftLeft = longDivisor;
+            long multiplicationBit = 1;
+            while(longDividend >= divisorShiftLeft<<1){
+                divisorShiftLeft <<= 1;
+                multiplicationBit <<= 1;
             }
-            longDividend -= temp;
-            quotient += multiplicator;
+            longDividend -= divisorShiftLeft;
+            quotient += multiplicationBit;
         }
-        return sign==1 ? quotient : -quotient;
+        boolean oneNegativOnePositiv = (dividend < 0) ^ (divisor < 0);
+        return oneNegativOnePositiv ? -quotient : quotient;
     }
 }
