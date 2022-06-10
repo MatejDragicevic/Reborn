89eecf93a3724b373bb5b22ce4e2aa399bd3bf35
MatejDragicevic <matej.dragicevic.work@gmail.com>
2022-06-10 12:54:38 +0200

    LC13

---
+++ leetcodeJava/LC13/RomanToInteger.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+        
+        int sum = 0;
+        int prev = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int current = romanMap.get(s.charAt(i));
+            sum += prev > current ? -current : current;
+            prev = current;
+        }
+        return sum;
+    }
+}
